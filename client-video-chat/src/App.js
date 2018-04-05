5679555553a8014704350d7248efde83a710d6af
jimmybengtsson <jb223pu@student.lnu.se>
2018-04-05 14:39:28 +0200

    Client: Added name form.

--- client-video-chat/src/App.js
+++ client-video-chat/src/App.js
@@ -3,13 +3,43 @@ import './App.css';
 
 import UserList from './components/UserList';
 import VideoChat from './components/VideoChat';
+import NameForm from './components/NameForm';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+        };
+        this.handleNameSubmit = this.handleNameSubmit.bind(this)
+    }
+
+    handleNameSubmit(value) {
+
+      this.setState({name: value,});
+      sessionStorage.setItem('name', value);
+    }
+
+    componentWillMount() {
+
+      if (sessionStorage.getItem('name')) {
+
+          this.setState({name: sessionStorage.getItem('name'),});
+      }
+    }
+
   render() {
     return (
       <div className="App">
-          <UserList/>
-          <VideoChat/>
+          {this.state.name ? (
+              <div>
+                  <UserList/>
+                  <VideoChat/>
+              </div>
+          ) : (
+              <NameForm handleNameSubmit={this.handleNameSubmit}/>
+          )}
       </div>
     );
   }
---
+++ client-video-chat/src/components/NameForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+class NameForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({name: event.target.value});
+    }
+
+    handleSubmit() {
+        this.props.handleNameSubmit(this.state.name);
+    }
+
+
+    render() {
+        return (
+            <div className="NameForm">
+                <input value={this.state.name} type="text" onChange={this.handleChange}/>
+                <button onClick={this.handleSubmit}>Submit</button>
+            </div>
+        );
+    }
+}
+
+export default NameForm;
