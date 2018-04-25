597ac7097e231dcf2f44c722bb73cad41a1d4669
Sofia Kristiansen <sofiakristiansen@Sofias-MacBook-Pro.local>
2018-04-25 17:54:37 +0200

    Created tests for ChangePassword.js.

--- client/__tests__/component/settings/user/ChangePassword.test.js
+++ client/__tests__/component/settings/user/ChangePassword.test.js
@@ -1,10 +1,130 @@
 import React from 'react';
+import Enzyme from 'enzyme';
+import {mount, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import ChangePassword from '../../../../src/component/settings/user/ChangePassword';
-import renderer from 'react-test-renderer';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 
-test('ChangePassword renders without crashing', () => {
-  const tree = renderer.create(
-    <ChangePassword />
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('ChangePassword', () => {
+  const props = {
+    openSnackBar: jest.fn()
+  };
+
+  const component = shallow(
+    <ChangePassword {...props}/>
   );
-  expect(tree.toJSON()).toMatchSnapshot();
+
+  it('renders without crashing', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+
+  describe('handleChange', () => {
+    let event = {
+      target: {
+        value: 'Password123'
+      }
+    }
+
+    it('should set password to Password123', () => {
+      component.instance().handleChange('password')(event);
+      const passwordState = component.state('password');
+
+      expect(passwordState).toEqual('Password123');
+    });
+
+    it('should set passwordConfirm to Password123', () => {
+      component.instance().handleChange('passwordConfirm')(event);
+      const passwordConfirmState = component.state('passwordConfirm');
+
+      expect(passwordConfirmState).toEqual('Password123');
+    });
+  });
+
+
+  describe('handleSubmit', () => {
+    it('should return "Formuläret ej korrekt ifyllt!" when password is missing', (done) => {
+      const props1 = {
+        openSnackBar: jest.fn()
+      };
+
+      const component1 = shallow(
+        <ChangePassword {...props1}/>
+      );
+
+      component1.setState({
+        password: '',
+        passwordConfirm: 'Password123'
+      });
+
+      component1.instance().handleSubmit();
+
+      expect(props1.openSnackBar).toHaveBeenCalledTimes(1);
+      done();
+    });
+
+    it('should return "Formuläret ej korrekt ifyllt!" when passwordConfirm is missing', (done) => {
+      const props2 = {
+        openSnackBar: jest.fn()
+      };
+
+      const component2 = shallow(
+        <ChangePassword {...props2}/>
+      );
+
+      component2.setState({
+        password: 'Password123',
+        passwordConfirm: ''
+      });
+
+      component2.instance().handleSubmit();
+
+      expect(props2.openSnackBar).toHaveBeenCalledTimes(1);
+      done();
+    });
+
+    it('should return "Lösenorden matchar inte!" when password and passwordConfirm are different', (done) => {
+      const props3 = {
+        openSnackBar: jest.fn()
+      };
+
+      const component3 = shallow(
+        <ChangePassword {...props3}/>
+      );
+
+      component3.setState({
+        password: 'Password123',
+        passwordConfirm: 'password1'
+      });
+
+      component3.instance().handleSubmit();
+
+      expect(props3.openSnackBar).toHaveBeenCalledTimes(1);
+      done();
+    });
+
+    //TODO: Rewrite this test when sendRequest is implemented.
+    it('should call sendRequest', (done) => {
+      const props4 = {
+        openSnackBar: jest.fn()
+      };
+
+      const component4 = shallow(
+        <ChangePassword {...props4}/>
+      );
+
+      component4.setState({
+        password: 'Password123',
+        passwordConfirm: 'Password123'
+      });
+
+      component4.instance().handleSubmit();
+
+      expect(props4.openSnackBar).toHaveBeenCalledTimes(0);
+      done();
+    });
+  });
 });
