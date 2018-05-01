55b1377d3ba3575dbf656836fa7f26042d49dcad
jimmybengtsson <jb223pu@student.lnu.se>
2018-05-01 16:14:56 +0200

    Server: Updated Cors settings.

--- client/src/utils/SignalR.js
+++ client/src/utils/SignalR.js
@@ -2,25 +2,28 @@ import {AzureServerUrl} from './Config'
 import { HubConnection } from '@aspnet/signalr'
 
 const chatServerUrl = AzureServerUrl + '/chat'
+const chatLocalUrl = 'http://localhost:49873/chat'
 
 let connection
 
-export const initChat = (token) => {
-  connection = new HubConnection(chatServerUrl + '?token=' + token)
-  return connection.start()
+export const createSignalR = (token) => {
+  connection = new HubConnection(chatLocalUrl + '?token=' + token)
+  return connection
 }
 
-export const createChatGroup = (groupName, token) => {
-  connection = new HubConnection(chatServerUrl + '?token=' + token)
-  return connection.start({ withCredentials: false })
+export const initChat = (connection) => {
+  return connection.start({ withCredentials: true })
+}
+
+export const createChatGroup = (connection, groupName) => {
+  return connection.start({ withCredentials: true })
     .then((response) => {
       console.log(response)
       connection.invoke('joinGroup', groupName)
     })
 }
 
-export const addUserToChat = (name, group, token) => {
-  connection = new HubConnection(chatServerUrl + '?token=' + token)
+export const addUserToChat = (connection, name, group) => {
   return connection.on('userAddedToGroup', (name, group) => {
     // Code here
   })
